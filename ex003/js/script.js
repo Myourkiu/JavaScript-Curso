4707ca97560116a486de26744fd0a2d9f180b6dc
Pedro Figueiredo <pedrofigsesteves@gmail.com>
2022-12-26 20:15:25 -0300

    exercicios 1-3

---
+++ ex003/js/script.js
@@ -0,0 +1,16 @@
+var teste = 1
+
+var nome = "Pedro"
+
+console.log(teste)
+
+teste = "Matheus"
+
+console.log(teste)
+console.log(nome)
+
+var $nome = "pasdslkd"
+var _nome = "kasjdna"
+
+console.log($nome)
+console.log(_nome)
